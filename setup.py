c8f59eb58bbaafa6dff3fb491fb1826446dfb769
David A. Parry <david.parry@igmm.ed.ac.uk>
2017-11-28 12:00:22 +0000

    add setup script

---
+++ setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+setup(
+    name = "bedfinder",
+    packages = [""],
+    version = "0.1.1",
+    description = "Read BED genomic interval files into memory and search.",
+    author = "David A. Parry",
+    author_email = "david.parry@igmm.ed.ac.uk",
+    url = "https://github.com/gantzgraf/bedfinder",
+    download_url = 'https://github.com/gantzgraf/bedfinder/archive/0.1.1.tar.gz',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    install_requires=[],
+    python_requires='>=3',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        ],
+)
